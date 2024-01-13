2cb616067a08d57dc6528f5c57bd70d9f04140db
Vinícius Bastos <viniciusbsilva57@gmail.com>
2024-01-13 09:41:15 -0300

    Create scroll to function for navbar

---
+++ assets/js/scrollTo.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll("header a");
+
+    links.forEach((link) => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
+});
